f79195ee17a305ca730cf75d3abad173622e0bb5
ChiFu <100756926+ChiFu0129@users.noreply.github.com>
2022-03-21 11:24:49 +0800

    update app.js file

--- 0319_TODOApp練習/app.js
+++ 0319_TODOApp練習/app.js
@@ -1,26 +1,26 @@
 // 程式碼寫這裡
-const addBtn = document.querySelector("#addBtn");
-const taskInput = document.querySelector("#taskInput");
+const addBtn = document.querySelector("#addBtn")
 
 addBtn.addEventListener("click", function () {
-  const todoitem = document.createElement("li");
-  todoitem.classList.add("todo-item");
-  const item = document.createElement("span");
-  item.classList.add("item");
-  const closeBtn = document.createElement("button");
-  closeBtn.classList.add("closeBtn");
-  closeBtn.textContent = "X";
+  const todoitem = document.createElement("li")
+  todoitem.classList.add("todo-item")
+  const item = document.createElement("span")
+  item.classList.add("item")
+  const closeBtn = document.createElement("button")
+  closeBtn.classList.add("closeBtn")
+  closeBtn.textContent = "X"
 
-  const todolist = document.querySelector(".todo-list");
-  todolist.appendChild(todoitem);
-  todoitem.appendChild(item);
-  todoitem.appendChild(closeBtn);
+  const todolist = document.querySelector(".todo-list")
+  todolist.appendChild(todoitem)
+  todoitem.appendChild(item)
+  todoitem.appendChild(closeBtn)
 
-  item.textContent = taskInput.value;
+  const taskInput = document.querySelector("#taskInput")
+  item.textContent = taskInput.value
 
-  todolist.insertAdjacentElement("afterbegin", todoitem);
+  todolist.insertAdjacentElement("afterbegin", todoitem)
 
   closeBtn.addEventListener("click", function () {
-    todoitem.remove();
-  });
-});
+    todoitem.remove()
+  })
+})
