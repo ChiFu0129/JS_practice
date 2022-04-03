014bfa3a8f21b2b0c4f76be32d9544f67b18c1fe
ChiFu <100756926+ChiFu0129@users.noreply.github.com>
2022-04-04 00:46:52 +0800

    update jsprac.js file

--- 0310_迴圈練習/jsprac.js
+++ 0310_迴圈練習/jsprac.js
@@ -1,13 +1,16 @@
+//九九乘法表：
 for (x = 1; x <= 9; x = x + 1) {
   for (y = 1; y <= 9; y = y + 1) console.log(x + " X " + y + " = " + x * y);
 }
 
 console.log("abc".repeat(3));
 
+//基本款：
 for (z = 1; z <= 5; z = z + 1) {
   console.log("*".repeat(z));
 }
 
+//進階款：
 for (i = 1; i <= 5; i = i + 1) {
   console.log(" ".repeat(5 - i) + "*".repeat(i) + "*".repeat(i - 1));
 }
